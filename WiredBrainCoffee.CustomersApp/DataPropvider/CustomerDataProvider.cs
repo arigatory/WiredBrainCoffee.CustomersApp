4fb3159c63518e82300d76114ca9003747289c19
Иван Иван <remillius_dies@outlook.com>
2020-08-24 20:44:20 +0300

    5 Accessing Objects from Code

---
+++ WiredBrainCoffee.CustomersApp/DataPropvider/CustomerDataProvider.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.DataPropvider
+{
+    public class CustomerDataProvider
+    {
+        private static readonly string _customerFileName = "customers.json";
+        private static readonly StorageFolder _localFolder = ApplicationData.Current.LocalFolder;
+        
+        public async Task<IEnumerable<Customer>> LoadCustomersAsync()
+        {
+            var storageFile = await _localFolder.TryGetItemAsync(_customerFileName) as StorageFile;
+            List<Customer> customerList = null;
+
+            if (storageFile == null)
+            {
+                customerList = new List<Customer>
+                {
+                    new Customer{FirstName="Thomas",LastName="Huber",IsDeveloper=true },
+                    new Customer{FirstName="Ivan",LastName="Panchenko",IsDeveloper=true },
+                    new Customer{FirstName="Andrew",LastName="Ivanov"},
+                    new Customer{FirstName="Vladimir",LastName="Volovits",IsDeveloper=true },
+                    new Customer{FirstName="Anna",LastName="Karennina"},
+                    new Customer{FirstName="Dmitry",LastName="Popov"},
+                    new Customer{FirstName="Max",LastName="Mad",IsDeveloper=true },
+                };
+            }
+            else
+            {
+                using (var stream = await storageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    using (var dataReader = new DataReader(stream))
+                    {
+                        await dataReader.LoadAsync((uint)stream.Size);
+                        var json = dataReader.ReadString((uint)stream.Size);
+                        customerList = JsonConvert.DeserializeObject<List<Customer>>(json);
+                    }
+                }
+            }
+            return customerList;
+
+        }
+
+        public async Task SaveCustomersAsync(IEnumerable<Customer> customers)
+        {
+            var storageFile = await _localFolder.CreateFileAsync(_customerFileName, CreationCollisionOption.ReplaceExisting);
+            using (var stream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (var dataWriter = new DataWriter(stream))
+                {
+                    var json = JsonConvert.SerializeObject(customers, Formatting.Indented);
+                    dataWriter.WriteString(json);
+                    await dataWriter.StoreAsync();
+                }
+            }
+        }
+    }
+}
--- WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -1,29 +1,59 @@
 ﻿using System;
+using System.Linq;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
-
+using WiredBrainCoffee.CustomersApp.DataPropvider;
+using WiredBrainCoffee.CustomersApp.Model;
 
 namespace WiredBrainCoffee.CustomersApp
 {
    
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
         }
 
-        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Custormer added!");
-            await messageDialog.ShowAsync();
+            customerListView.Items.Clear();
+
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+        private async void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(customerListView.Items.OfType<Customer>());
+            deferral.Complete();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
         }
 
         private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
         {
-
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer!=null)
+            {
+                customerListView.Items.Remove(customer);
+            }
         }
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
@@ -35,6 +65,36 @@ namespace WiredBrainCoffee.CustomersApp
             Grid.SetColumn(customerListGrid, newColumn);
 
             moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer!=null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
         }
     }
 }
